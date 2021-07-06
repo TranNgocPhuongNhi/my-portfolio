d062bebffab119749e84878f22f8332c07b9182c
Trần Ngọc Phương Nhi <tranngocphuongnhi0412@gmail.com>
2021-07-06 10:15:47 +0700

    update

---
+++ .history/resources/js/main_20210705220154.js
@@ -0,0 +1,157 @@
+// ======================================= Mobile Navbar ==================================
+const mobile_nav = document.querySelector('.header__nav-mobile');
+const mobile_nav = document.querySelector('.header__nav-list');
+
+mobile_nav.addEventListener('click', () => {
+
+})
+
+
+// ===================================== Scrolling nav ==================================
+
+const nav = document.querySelector("nav");
+window.addEventListener("scroll",() => {
+    const offset = window.pageYOffset;
+    if(offset > 20) {
+        nav.classList.add('nav-scroll');
+    }
+    else {
+        nav.classList.remove('nav-scroll');
+    }
+})
+
+
+// =========================================== To the top ===================================
+const backToTop = document.querySelector('.footer__to-top');
+window.addEventListener("scroll", () => {
+    const offset = window.pageYOffset;
+    if(offset > 80) {
+        backToTop.style.display = 'block';
+    }
+    else {
+        backToTop.style.display = 'none';
+    }
+})
+
+
+
+// ===================================== Circle Process Bar ===============================
+
+$('.skill-item').circleProgress({fill: {gradient: ["red", "orange"]}})
+.on('circle-animation-progress', function (event, progress) {
+    var data_value = $(this).attr('data-value');
+    $(this).find('strong').html(Math.round(data_value * 100 * progress) + '<span class="percent__skills">%</span>');
+});
+
+// ============================================= Particles.js ==================================================
+particlesJS("header", {
+    "particles": {
+      "number": {
+        "value": 180,
+        "density": {
+          "enable": true,
+          "value_area": 777
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#F4B301",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
